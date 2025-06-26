644f82f54944dc5c63f3332dde5f7cb79fd812e0
MANNEPALLI BALA PRAHARSHA <balapraharsha.m@gmail.com>
2025-06-27 04:59:18 +0530

    Add files via upload

---
+++ PROJECT EXECUTABLE FILES/app.py
@@ -0,0 +1,47 @@
+import os
+import numpy as np
+import cv2
+from flask import Flask, request, render_template, redirect, url_for
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+import base64
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+os.makedirs("static", exist_ok=True)
+
+model = load_model(r"C:\Users\balap\vscode programs\Blood-cell\Blood_cell.h5")
+class_labels = ['eosinophil', 'lymphocyte', 'monocyte', 'neutrophil']
+
+def predict_image_class(image_path, model):
+    img = cv2.imread(image_path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_resized = cv2.resize(img_rgb, (224, 224))
+    img_preprocessed = preprocess_input(img_resized.reshape((1, 224, 224, 3)))
+    predictions = model.predict(img_preprocessed, verbose=0)
+    predicted_class_idx = np.argmax(predictions, axis=1)[0]
+    predicted_class_label = class_labels[predicted_class_idx]
+    return predicted_class_label, img_rgb
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if file.filename == "":
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file_path = os.path.join("static", filename)
+            file.save(file_path)
+            predicted_class_label, img_rgb = predict_image_class(file_path, model)
+
+            _, img_encoded = cv2.imencode('.png', cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR))
+            img_str = base64.b64encode(img_encoded).decode('utf-8')
+
+            return render_template("result.html", class_label=predicted_class_label, img_data=img_str)
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
